563ee067745fc45c0aaafe0ef7de0414124139d3
ioanamadaras <ioanamadaras2000@gmail.com>
2025-11-13 02:59:17 +0200

    login route fix

--- MADARAS_IOANA_Lab2/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ MADARAS_IOANA_Lab2/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -91,7 +91,7 @@ namespace MADARAS_IOANA_Lab2.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl ??= Url.Content("/Books");
+            returnUrl ??= Url.Content("~/");
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
@@ -103,7 +103,7 @@ namespace MADARAS_IOANA_Lab2.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("/Books");
+            returnUrl ??= Url.Content("~/");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
