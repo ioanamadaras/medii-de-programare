68b36be0403cbe06281d2fe894cca9c07d96d87c
ioanamadaras <ioanamadaras2000@gmail.com>
2025-12-03 22:45:11 +0200

    WIP lab9

--- MadarasIoanaLab7/ListPage.xaml.cs
+++ MadarasIoanaLab7/ListPage.xaml.cs
@@ -1,12 +1,36 @@
-namespace MadarasIoanaLab7;
+﻿namespace MadarasIoanaLab7;
 using MadarasIoanaLab7.Models;
 
 public partial class ListPage : ContentPage
 {
+    ShopList sl;
     public ListPage()
     {
         InitializeComponent();
+        sl = (ShopList)this.BindingContext;
     }
+
+    async void OnDeleteItemButtonClicked(object sender, EventArgs e)
+    {
+        Product p;
+
+        if (listView.SelectedItem != null)
+        {
+            p = listView.SelectedItem as Product;
+            await App.Database.DeleteProductAsync(p);
+        }
+    }
+
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(
+            new ProductPage((ShopList)this.BindingContext)
+            {
+                BindingContext = new Product()
+            }
+        );
+    }
+
 
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
@@ -24,4 +48,14 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
 }
