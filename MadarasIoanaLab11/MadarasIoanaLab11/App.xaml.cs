22c140f44d21dd552acaf4ade5f27de250e2bc71
Andrei Vascul <andrei.vascul@stud.ubbcluj.ro>
2025-12-19 02:19:28 +0200

    lab11 final

---
+++ MadarasIoanaLab11/MadarasIoanaLab11/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MadarasIoanaLab11.Data;
+
+namespace MadarasIoanaLab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+
+        public App()
+        {
+            InitializeComponent();
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
--- MadarasIoanaLab7/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MadarasIoanaLab7")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MadarasIoanaLab7.Pages")]
